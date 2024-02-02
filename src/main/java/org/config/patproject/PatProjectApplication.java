0ed83c51709e7dceea695c54517897ce0257f78f
Bogdan <b.synenko@vector-inc.dev>
2024-02-02 17:44:28 +0200

    Initial commit

---
+++ src/main/java/org/config/patproject/PatProjectApplication.java
@@ -0,0 +1,13 @@
+package org.config.patproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/config/patproject/PatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.config.patproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
